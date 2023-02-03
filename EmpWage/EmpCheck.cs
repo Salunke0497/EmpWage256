f29e24c5e34cf0e099b6e0dbf749bde11f16c375
Salunke0497 <113031652+Salunke0497@users.noreply.github.com>
2023-02-03 09:56:28 +0530

    welcome

---
+++ EmpWage/EmpCheck.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpWage
+{
+    public class EmpCheck
+    {
+        public void AttendanceCheck() 
+        {
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if(empCheck==0)
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Present");
+            }
+        }
+    }
+}
---
+++ EmpWage/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpWage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EmpCheck obj= new EmpCheck();
+            obj.AttendanceCheck();
+            Console.ReadLine();
+        }
+    }
+}
